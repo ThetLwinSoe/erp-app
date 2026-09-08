340fb7b16eff69e6b1a76dcf273aa51ddd652151
ThetLwinSoe <thetlwinsoe@gmail.com>
2026-09-08 12:56:02 +0700

    Moved to new path

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\My Projects\erp-app"
+export "FLUTTER_APPLICATION_PATH=D:\My Projects\CoreFlow ERP\Source Code\erp-app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
